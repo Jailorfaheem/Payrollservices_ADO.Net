c16b11a5a8843bd5a4f64f30d1f1eb97153d6e93
faheemjailor <faheemjailor0055>
2021-11-28 21:48:23 +0530

    Faheem UC1 for creating database

---
+++ PayrollServices_ADO.Net/PayrollServices_ADO.net/EmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace PayrollService_ADO.Net
+{
+    public class EmployeeRepository
+    {
+        public static string connectionString = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = payroll_services;";
+        //creating object of sqlconnection class
+        SqlConnection sqlConnection = new SqlConnection(connectionString);
+    }
+}
