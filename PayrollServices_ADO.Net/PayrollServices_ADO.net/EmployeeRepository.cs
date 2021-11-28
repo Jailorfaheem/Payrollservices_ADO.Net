ce3febe0748a7a9edccc2798bca448d3bf55ad2d
faheemjailor <faheemjailor0055>
2021-11-28 21:59:07 +0530

    Faheem UC2 for retrieving employee details

--- PayrollServices_ADO.Net/PayrollServices_ADO.net/EmployeeRepository.cs
+++ PayrollServices_ADO.Net/PayrollServices_ADO.net/EmployeeRepository.cs
@@ -13,5 +13,52 @@ namespace PayrollService_ADO.Net
         public static string connectionString = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = payroll_services;";
         //creating object of sqlconnection class
         SqlConnection sqlConnection = new SqlConnection(connectionString);
+        public void GetAllEmployeeDetails()
+        {
+            try
+            {
+                EmployeeModel model = new EmployeeModel();
+                string query = @"select * from employee_payroll;";         //query for fetching the data from database                
+                SqlCommand command = new SqlCommand(query, sqlConnection); //command object to execute query on given database
+                sqlConnection.Open();
+                SqlDataReader reader = command.ExecuteReader();            //returning as rows
+
+                if (reader.HasRows)                              //condition to check rows present or not in selected database
+                {
+                    //if rows are present then read the rows 
+                    while (reader.Read())
+                    {
+                        model.EmployeeId = Convert.ToInt32(reader["Id"] == DBNull.Value ? default : reader["Id"]);
+                        model.EmployeeName = Convert.ToString(reader["Name"] == DBNull.Value ? default : reader["Name"]);
+                        model.Salary = Convert.ToDouble(reader["Salary"] == DBNull.Value ? default : reader["Salary"]);
+                        model.StartDate = (DateTime)(reader["StartDate"] == DBNull.Value ? default : reader["StartDate"]);
+                        model.Gender = Convert.ToString(reader["Gender"] == DBNull.Value ? default : reader["Gender"]);
+                        model.PhoneNumber = Convert.ToInt32(reader["Phone"] == DBNull.Value ? default : reader["Phone"]);
+                        model.Address = Convert.ToString(reader["Address"] == DBNull.Value ? default : reader["Address"]);
+                        model.Department = Convert.ToString(reader["Department"] == DBNull.Value ? default : reader["Department"]);
+                        model.TaxablePay = Convert.ToDouble(reader["Taxable_Pay"] == DBNull.Value ? default : reader["Taxable_Pay"]);
+                        model.IncomeTax = Convert.ToDouble(reader["Income_Tax"] == DBNull.Value ? default : reader["Income_Tax"]);
+                        model.NetPay = Convert.ToDouble(reader["Net_Pay"] == DBNull.Value ? default : reader["Net_Pay"]);
+
+                        Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9}", model.EmployeeId, model.EmployeeName, model.Salary, model.StartDate, model.Gender, model.PhoneNumber, model.Address, model.TaxablePay, model.IncomeTax, model.NetPay);
+                        Console.WriteLine("\n");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows present");
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
     }
+
 }
